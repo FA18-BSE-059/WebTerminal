36aa59bbe1821a88b0c56aba027163abe63041af
Tahir Mahmood <30719791+tahirmahm123@users.noreply.github.com>
2021-06-22 11:51:32 +0500

    check-in

---
+++ models/user.js
@@ -0,0 +1,31 @@
+var mongoose = require('mongoose');
+
+userScehma = new mongoose.Schema({
+    name: {
+        type: String,
+        required: "Name is required."
+    },
+    gender: {
+        type: String,
+        enum: [
+            'Male', 'Female', 'Others'
+        ],
+        required: 'Gender is Required'
+    },
+    email: {
+        type: String,
+        required: 'Email is required'
+    },
+    password: {
+        type: String,
+        required: 'Password is required'
+    }
+});
+
+// Custom validation for email
+userScehma.path('email').validate((val) => {
+    let emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return emailRegex.test(val);
+}, 'Invalid E-mail Format.');
+const User = mongoose.model('User',userScehma);
+module.exports = User
